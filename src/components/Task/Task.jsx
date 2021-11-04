515484144c51185fb27e7b2c388dfa3f7dc79d25
Tagir <tagir142@gmail.com>
2021-11-04 18:16:53 +0500

    комплит таск выполнено

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,9 +1,16 @@
-import { TaskWrapper, TaskText } from './Task.styles'
+import { TaskWrapper, TaskText, TaskChecked } from './Task.styles'
+
+import { useState } from 'react'
 
 export const Task = ({ task }) => {
+  const [checked, setChecked] = useState(false)
+  const handleClick = () => setChecked(!checked)
+
   return (
     <TaskWrapper>
-        <TaskText>{task}</TaskText>
+        <TaskText className={checked ? 'completeTask' : 'nonCompleteTask'}
+                  id={checked ? 'line-through' : 'none'}>{task}</TaskText>
+        <TaskChecked type="checkbox" checked={checked} onChange={handleClick}/>
     </TaskWrapper>
   )
 }
--- src/components/Task/Task.styles.js
+++ src/components/Task/Task.styles.js
@@ -1,11 +1,24 @@
 import styled from 'styled-components'
 
-export const TaskWrapper = styled.div`
+export const TaskWrapper = styled.form`
   background-color: darkseagreen;
   padding: 2px 2px 2px 2px;
   width: 300px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `
-export const TaskText = styled.div`
-      text-align: center;
-      background-color: white;
+export const TaskText = styled.label`
+  text-align: center;
+  padding-left: 4px;
+  background-color: white;
+  width: 100%;
+  font-size: 22px;
+  text-decoration: ${props => props.id};
+  
+    
+`
+
+export const TaskChecked = styled.input`
+      
 `
