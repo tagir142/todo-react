e40bf6297d1a764e3f6c41cba9c1b8f28a5e8dd3
tagir <tagir142@gmail.com>
2021-11-28 17:33:26 +0500

    Все пашет

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -7,8 +7,6 @@ import { nanoid } from 'nanoid'
 export const List = () => {
   const [tasks, setTasks] = useState([])
 
-  // const [tasksEditing, setTasksEditing] = useState('')
-
   const addTodo = (text) => {
     const id = nanoid()
     const date = new Date().toLocaleString()
@@ -22,11 +20,12 @@ export const List = () => {
   }
   const deleteTask = (id) => {
     setTasks([...tasks.filter((item) => item.id !== id)])
+    localStorage.clear()
   }
 
   useEffect(() => {
     const json = JSON.stringify(tasks)
-    if (tasks.length !== 0) {
+    if (tasks.length > 0) {
       localStorage.setItem('tasks', json)
     }
   }, [tasks])
@@ -36,34 +35,9 @@ export const List = () => {
     const savedTasks = JSON.parse(jsonParse)
     if (savedTasks) {
       setTasks(savedTasks)
-      console.log(jsonParse)
     }
   }, [])
-  /* useEffect(() => {
-    const json = JSON.stringify(tasks)
-    localStorage.setItem('tasks', json)
-    const jsonParse = localStorage.getItem('tasks')
-    const savedTasks = JSON.parse(jsonParse)
 
-    // const savedTasks = JSON.parse(localStorage.getItem('tasks'))
-    if (tasks.length !== 0) {
-      localStorage.getItem('tasks')
-    } else {
-      localStorage.setItem('tasks', json)
-    }
-  }, [tasks])
-
-   const useLocalStorageList = (key, defaultValue) => {
-    const [state, setState] = useState(() => JSON.parse(localStorage.getItem(key) || defaultValue))
-    useEffect(() => {
-      localStorage.setItem(key, JSON.stringify(tasks))
-    }, [tasks])
-    return [state, setState]
-  }
-
-  const [resultId, setResultId] = useLocalStorageList('resultId', 'true')
-  const toggle = () => setResultId(!resultId)
-*/
   return (
 
       <ListWrapper>
